35890f58202d39d8793bf3b2b63485801a88a837
Israel Boudoux <israel.boudoux@wit-software.com>
2019-04-13 23:46:15 +0100

    Removing target directory

--- target/src/main/java/edu/israel/java8/Function.java
+++
@@ -1,9 +0,0 @@
-package edu.israel.java8;
-
-public class Function {
-
-	public static void main(String[] args) {
-		
-	}
-	
-}
--- target/src/main/java/edu/israel/java8/Supplier.java
+++
@@ -1,5 +0,0 @@
-package edu.israel.java8;
-
-public class Supplier {
-
-}
